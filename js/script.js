fafede416f461169f8d5b881c1caf00e04e49e64
ramya3184 <ramya3184@gmail.com>
2018-12-08 14:06:09 -0800

    add epicodusworkshop to git

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// alert("Hello World!");
+// alert(2 + 2);
+// alert(4 * "2");
+// alert(8 + "2");
+// $(document).ready(function() {
+//   // alert("jQuery is working");
+//   alert($("#css-item").text());
+// })
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
